d02508fb9576e0ba93239d2b2bff77e45ee89a15
Natheesan30 <2020sp092@univ.jfn.ac.lk>
2025-12-24 17:06:00 +0530

    use the Elevated button and how to reset the form.

--- dice_application/lib/main.dart
+++ dice_application/lib/main.dart
@@ -6,10 +6,10 @@ void main() {
 
 class ExpenseApp extends StatelessWidget {
   const ExpenseApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home:  AddExpenseForm());
+    return const MaterialApp(home: AddExpenseForm());
   }
 }
 
@@ -19,12 +19,17 @@ class ExpenseHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Expense Tracker'),),
-      body: const Column(children: [
-                  ExpenseCard(title: "Groceries", date: "Dec 22, 2025", amount: 250.0),
-                  ExpenseCard(title: "Rent", date: "Dec 1, 2025", amount: 1200.0), 
-                  ExpenseCard(title: "Utilities", date: "Dec 15, 2025", amount: 150.0)],) 
-    );
+        appBar: AppBar(
+          title: const Text('Expense Tracker'),
+        ),
+        body: const Column(
+          children: [
+            ExpenseCard(
+                title: "Groceries", date: "Dec 22, 2025", amount: 250.0),
+            ExpenseCard(title: "Rent", date: "Dec 1, 2025", amount: 1200.0),
+            ExpenseCard(title: "Utilities", date: "Dec 15, 2025", amount: 150.0)
+          ],
+        ));
   }
 }
 
@@ -43,26 +48,43 @@ class ExpenseCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.all(20),
-      elevation: 5,
-      child: Padding(padding: EdgeInsets.all(10), child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [Text(title, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                        SizedBox(height: 5,),
-                         Text(date, style: TextStyle(fontSize: 16, color: Colors.grey[600]))],),
-            Container(
-              child: Text("\$${amount.toStringAsFixed(2)}", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.green.shade400),),
-              decoration: BoxDecoration(border: Border.all(color: Colors.blue.shade400), borderRadius: BorderRadius.circular(5)),
-              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-            ),
-         ],
-      ),
-    )
-         );
+        margin: EdgeInsets.all(20),
+        elevation: 5,
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(title,
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(date,
+                      style: TextStyle(fontSize: 16, color: Colors.grey[600]))
+                ],
+              ),
+              Container(
+                child: Text(
+                  "\$${amount.toStringAsFixed(2)}",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green.shade400),
+                ),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blue.shade400),
+                    borderRadius: BorderRadius.circular(5)),
+                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+              ),
+            ],
+          ),
+        ));
   }
 }
 
@@ -73,54 +95,105 @@ class AddExpenseForm extends StatefulWidget {
   State<AddExpenseForm> createState() => _AddExpenseFormState();
 }
 
-class _AddExpenseFormState extends State<AddExpenseForm> { // This is the state class
+class _AddExpenseFormState extends State<AddExpenseForm> {
+  // This is the state class
 
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
 
+  final _formKey = GlobalKey<FormState>();
+
   void showDate() async {
     final pickedDate = await showDatePicker(
-      context: context,           // This context already define in the state class
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2024),  
+      context: context, // This context already define in the state class
+      initialDate: _selectedDate == null ? DateTime.now() : _selectedDate!,
+      firstDate: DateTime(2024),
       lastDate: DateTime.now(),
     );
-    if(pickedDate != null) {
-      _selectedDate = pickedDate;
-  }
+    if (pickedDate != null) {
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    }
   }
 
-  void submitForm(){
+  void submitForm() {
+    if (!_formKey.currentState!.validate() || _selectedDate == null) {
+      return;
+    }
     final title = _titleController.text;
     final amount = double.tryParse(_amountController.text);
 
     print("Title: $title, Amount: $amount, Date: $_selectedDate");
   }
 
+  void resetForm(){
+    _titleController.clear();
+    _amountController.clear();
+    setState(() {
+      _selectedDate = null;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Add Expense')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: Form(
+        key: _formKey,
         child: Column(
           children: [
             TextFormField(
               controller: _titleController,
               decoration: const InputDecoration(labelText: 'Title'),
+              validator: (value) => value == null || value.isEmpty
+                  ? 'Please enter a title'
+                  : null,
             ),
             TextFormField(
               controller: _amountController,
               decoration: const InputDecoration(labelText: 'Amount'),
               keyboardType: TextInputType.number,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter an amount';
+                }
+                final amount = double.tryParse(value);
+                if (amount == null || amount <= 0) {
+                  return 'Please enter a valid amount';
+                }
+                return null;
+              },
             ),
+            Text(_selectedDate == null
+                ? "No Date Chosen!"
+                : "Picked Date: ${_selectedDate!.toLocal().toString().split(' ')[0]}"),
             const SizedBox(height: 20),
             TextButton(
-              onPressed: showDate,  // Connect the function here
-              child: Text( "Select Date"),
+              onPressed: showDate, // Connect the function here
+              child: Text("Select Date"),
             ),
-            ElevatedButton(onPressed: () => submitForm(), child: Text("Add Expense"))
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => submitForm(),
+                  child: Text("Add Expense"),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue.shade500,
+                      foregroundColor: Colors.white),
+                ),
+                SizedBox(width: 25),
+                ElevatedButton(
+                  onPressed: () => resetForm(),
+                  child: Text('Reset'),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red.shade500,
+                      foregroundColor: Colors.white),
+                ),
+              ],
+            )
           ],
         ),
       ),
